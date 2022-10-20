69f8fabc0d5b9e78e90d2b79f84c2bf9bb86e697
Yannick <33485945+YannickAaron@users.noreply.github.com>
2022-10-20 14:23:19 +0100

    make sure sda is mounted correctly every time!

--- format.sh
+++ format.sh
@@ -1,9 +1,15 @@
+#get name of external drive
+echo "Enter the name of the external drive"
+read drive
+#mount external drive
+sudo mount /dev/$drive /media/$drive
+
 #check if dev/sdb is already formatted
-if [ -e /dev/sdb ]; then
+if [ -e /dev/sda ]; then
     echo "sdb already formatted"
 else
     echo "formatting sdb"
-    mkfs.ext4 /dev/sdb
+    mkfs.ext4 /dev/sda
 fi
 
 #create /data mount point if it doesn't exist
@@ -15,9 +21,17 @@ else
 fi
 
 #check if dev/sdb is already mounted
-if grep -qs '/dev/sdb' /proc/mounts; then
+if grep -qs '/dev/sda' /proc/mounts; then
     echo "sdb already mounted"
 else
-    echo "mounting sdb"
-    mount /dev/sdb /data
+    echo "mounting sda"
+    mount /dev/sda /data
+fi
+
+#make sure that sda is mounted automatically
+if grep -qs '/dev/sda' /etc/fstab; then
+    echo "sda already in fstab"
+else
+    echo "adding sda to fstab"
+    echo "/dev/sda /data ext4 defaults 0 0" >> /etc/fstab
 fi
