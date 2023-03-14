85816a9544a639870c1e5760d91d845d1b0909f8
Yannick <33485945+YannickAaron@users.noreply.github.com>
2023-03-14 21:53:41 +0100

    ready

--- clone_repos.sh
+++ clone_repos.sh
@@ -6,10 +6,12 @@ REPOS_FILE="/maintenance/repos.txt"
 BASE_PATH="data/containers"
 
 while read -r repo; do
-  REPO_NAME=$repo
-  if [ "$REPO_NAME" = "." ] || [ "$REPO_NAME" = "" ]; then
+  REPO=$repo
+  if [ "$REPO" = "." ] || [ "$REPO" = "" ]; then
     continue
   fi
+  REPO_NAME=$(echo $REPO | awk -F'/' '{print $NF}')
+
   TARGET_PATH="$BASE_PATH/$REPO_NAME"
 
   if [ ! -d "$TARGET_PATH" ]; then
